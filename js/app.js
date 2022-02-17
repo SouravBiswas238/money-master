53475d95bf198598aa4d535c96e1e60f9a30fa35
SouravBiswas238 <souravbiswas427@gmail.com>
2022-02-17 16:43:19 +0600

    updated final commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Decliaring click calculate function
+// Initializing value
 const totalIncome = document.getElementById('total-income');
 const balence = document.getElementById('balence');
 const foodText = document.getElementById('food-cost');
@@ -8,7 +8,6 @@ const totalExpence = document.getElementById('total-expense');
 const remainingBalence = document.getElementById('remaining-balence');
 const totalSavingText = document.getElementById('total-saving');
 const savePercentText = document.getElementById('savePercent');
-
 
 // Function CAN cheak the value string or number
 function cheakNumber(inputValue) {
@@ -24,35 +23,30 @@ function cheakNumber(inputValue) {
         document.getElementById('positive-messege').style.display = "none";
         return true;
     }
-
 }
 // all value adding function
 function costValue(foodValue, rentValue, clothesValue) {
     let foodPrize = parseInt(foodValue);
     let rentPrize = parseInt(rentValue);
     let clothesPrize = parseInt(clothesValue);
-
     let totalCost = foodPrize + rentPrize + clothesPrize;
     totalExpence.innerText = totalCost;
-
 }
-
 //update Balence 
 function updateBalence() {
     const totalExp = parseInt(totalExpence.innerText);
     const newTotalIncome = parseInt(totalIncome.value);
     newBalence = newTotalIncome - totalExp;
     if (totalExp > newTotalIncome) {
-        // console.log("loss ho gia");
         document.getElementById('error-expense').style.display = "block";
         balence.innerText = 0;
     }
     else {
         document.getElementById('error-expense').style.display = "none";
         balence.innerText = newBalence;
-
     }
 }
+// Decliaring click calculate function
 document.getElementById('calculate-btn').addEventListener('click', function () {
     let isTotalIncome = cheakNumber(totalIncome.value);
     let isFoodText = cheakNumber(foodText.value);
@@ -72,12 +66,10 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     }
 });
 function savingPersentence() {
-
     const savePercent = parseInt(savePercentText.value);
     const newTotalIncome = parseInt(totalIncome.value);
     let savingAmount = (newTotalIncome * savePercent) / 100;
     const finalBalence = parseInt(balence.innerText);
-
     const isNumber = cheakNumber(savePercentText.value);
     if (isNumber) {
         document.getElementById('error-saving').style.display = "none";
@@ -87,7 +79,6 @@ function savingPersentence() {
             document.getElementById('error-balence').style.display = "block";
             remainingBalence.innerText = 0;
             totalSavingText.innerText = 0;
-
         }
         else {
             document.getElementById('error-balence').style.display = "none";
@@ -96,10 +87,9 @@ function savingPersentence() {
     }
     else {
         document.getElementById('error-saving').style.display = "block";
-
     }
 }
-
+// reset all value
 document.getElementById('reset-btn').addEventListener('click', function () {
     totalIncome.value = '';
     foodText.value = '';
@@ -115,10 +105,6 @@ document.getElementById('reset-btn').addEventListener('click', function () {
     document.getElementById('error-balence').style.display = "none";
     document.getElementById('error-saving').style.display = "none";
     document.getElementById('error-expense').style.display = "none";
-
-
-
-
 });
 
 
